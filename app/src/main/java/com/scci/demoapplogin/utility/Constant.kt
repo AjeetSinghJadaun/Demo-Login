507f0e414108d6d3685555230b50c4bc0df4ca38
AjeetSinghJadaun <ajeetsinghja@gmail.com>
2025-06-17 17:19:17 +0530

    host commit

--- app/src/main/java/com/scci/demoapplogin/utility/Constant.kt
+++ app/src/main/java/com/scci/demoapplogin/utility/Constant.kt
@@ -5,8 +5,8 @@ object Constant {
     const val IS_LOGIN = "isLogin"
     const val USER_DATA = "userData"
     const val LOGIN_DATA = "loginData"
-    const val CANCOLLECTIONPOINTSERVICES = "CanCollectionPointServices"
-    const val host = "https://gd9.scci.co.in/"
+    const val CANCOLLECTIONPOINTSERVICES = ""
+    const val host = ""
 
     const val NETWORK_NOTFOUND = "Internet is not enable in your device, Please enable!"
     const val HOST_NOTFOUND = "Server is not reachable or not responding, Please try later!"
